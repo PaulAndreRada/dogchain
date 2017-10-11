29a48ec4ee6442f6c68f61d4bc7babc8dfc2431b
Paul Andre Rada <paulriverarada@gmail.com>
2017-10-10 20:42:27 -0700

    Added the new_transactions method

--- blockchain.py
+++ blockchain.py
@@ -13,10 +13,31 @@ class Blockchain(object):
         # creates a new block and adds it to the chain
         pass
 
-    # create the method for a transaction?
-    def new_transaction(self): 
+
+    # create the method for a transaction
+    def new_transaction(self, sender, recipient, amount): 
+
+        """ 
+        Creates a new transaction to go into the next mined Block
+        
+        :param sender: <str> Address of the sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the block that will hold this transaction
+        """
+        
+        # Add the transaction into the list of the last transaction
+        self.current_transactions.append({ 
+          'sender': sender, 
+          'recipient': recipient, 
+          'amount': amount,
+       })
+
         # Adds a new transaction to the list of transactions
-        pass 
+        # in the old last block created (the new one)
+       return self.last_block['index'] + 1
+    
+
 
     @staticmethod
     def hash(block):
