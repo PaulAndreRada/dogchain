7dc9c23cf67bf0939e237eb64d6d91f2038f2966
Paul Andre Rada <paulriverarada@gmail.com>
2017-10-10 21:32:14 -0700

    Flushed out new_block, new_transaction() and hash()

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,8 @@
+import hashlib
+import json
+
+from time import time 
+from uuid import uuid4
 
 class Blockchain(object): 
     
@@ -8,10 +13,37 @@ class Blockchain(object):
         # create the transaction array
         self.current_transactions = [] 
 
-    # create the method for a new block?
-    def new_block(self):
+        # create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+
+    # create the method for a new block
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new Block in the Blockchain
+        
+        :param proof: <int> the proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> hash of previous Block
+        :return <dict> New Block
+        """
+        
         # creates a new block and adds it to the chain
-        pass
+        block = { 
+            'index': len(self.chain) + 1, 
+            'timestamp': time(), 
+            'transactions': self.current_transactions, 
+            'proof': proof, 
+            'previous_hash': previous_hash or self.hash(self.chain[-1]), 
+        }
+
+        # Reset the current list of transactions 
+        # its a class paul, remember it's part of the contructor scope, not js
+        self.current_transactions = []
+        
+        # add the new block to the chain & return it in order to chain the command 
+        self.chain.append(block)
+        return block
+
 
 
     # create the method for a transaction
@@ -36,13 +68,21 @@ class Blockchain(object):
         # Adds a new transaction to the list of transactions
         # in the old last block created (the new one)
         return self.last_block['index'] + 1
-    
-
 
     @staticmethod
     def hash(block):
         # Hashes a Block 
-        pass
+        """ 
+        Creates a SHA-256 hash of a Block
+        
+        :param block: <dict> Block
+        :return: <str>
+        """
+        
+        # Make sure the Dictionary is Ordered, or we'll have inconsistent hashes
+        # this part needs more breaking down
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def las_block(self):
