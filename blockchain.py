a2287f911d966d9c8a424d752f49628cb11c3153
Paul Andre Rada <paulriverarada@gmail.com>
2017-10-14 13:43:20 -0700

    Stage 3 - finished the consensus algorithm - http calls don't work though

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,10 @@
 import hashlib
 import json
-from textwrap import dedent
-from time import time 
+from time import time
+from urllib.parse import urlparse
 from uuid import uuid4
 
+import requests
 from flask import Flask, jsonify, request
 
 class Blockchain(object): 
@@ -14,10 +15,99 @@ class Blockchain(object):
         self.chain = []
         # create the transaction array
         self.current_transactions = [] 
+        # set additional nodes, to track other http connections
+        self.nodes = set()
 
         # create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
+    
+    # Registers all the connected nodes in a nodes array
+    def register_node( self, address): 
+        """ 
+        Add a new node to the list of nodes
+        
+        :param address: <str> Address of node. eg. 'http://192.168.0.5:5000'
+        :return: None
+        """ 
+        
+        # parse the URL and place it in the nodes array
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain): 
+        """
+        Determine if a given blockchain is valid
+        
+        :param chain: <list> A Blockchain
+        :return: <bool> True if valid, False if not
+        """
+        
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print(f"\n----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                # Break the chain, the hash has been corropted
+                return false
+
+            # Check that the proof of work is correct
+            if not self.valid_proof(last_block['proof'], blook['proof']):
+                # Break the chain, the proof has been corropted
+                return false
+            
+            # Make the new last block result be this block ( to jump to the next, next iteration )
+            last_block = block
+            # Go to the next index in the chain
+            current_index += 1
+            
+        # it's a valid chain,
+        return True
+
+    def resolve_conflicts(self): 
+
+        """
+        This is out Consensus Algorithm, it resolves conflics
+        by replacing our chain with he longest one in the network. 
+
+        It technically just updates all the chains that are shorter
+        than the one that created the latest block
+        
+        :return: <bool> True if our chain was replaced, False if not
+        """
+        
+        neighbours = self.nodes
+        new_chain = None
+        
+        # We're only looking for chains from all the nodes in our network
+        # this should be the next step to change
+        max_length = len(self.chain)
+        
+        # Grab and verify the chains from all the nodes in the network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+            
+            # if the response is successful, grab the chain and it's length
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                
+                # Check if the length is longer and the chain is valid 
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain: 
+            self.chain = new_chain
+            return True
+        
+        return False
 
     # create the method for a new block
     def new_block(self, proof, previous_hash=None):
@@ -192,5 +282,51 @@ def full_chain():
     }
     return jsonify(response), 200
 
+@app.route('/nodes/register',methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+    nodes = values.get('nodes')
+
+    # is this a base case?
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    # I think this is recursive, i confus...
+    for node in nodes:
+        blockchain.register_nodes(node)
+
+    response = { 
+        'message': 'New nodes have been added', 
+        'total_nodes': list(blockchain.nodes), 
+    }
+    
+    return jsonify(response), 201
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus(): 
+    replaced = blockchain.resolve_conflicts()
+    
+    if replaced: 
+        response = { 
+            'message': 'Our chain was replaced', 
+            'new_chain': blockchain.chain
+    }
+    else: 
+        response = {
+            'message': 'Our chain was replaced', 
+            'new_chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='0.0.0.0', port=port)
+
